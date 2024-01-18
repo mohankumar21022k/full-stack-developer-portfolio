acbe8cdaf4141dbf0314efea714af1bdf69236d6
Mohan Kumar A <mohankumar21022k@gmail.com>
2024-01-18 18:33:21 +0530

    resend-email-configure-fix

--- src/action/sendEmail.ts
+++ src/action/sendEmail.ts
@@ -26,7 +26,7 @@ export const sendEmail = async (formData: FormData) => {
     let data;
     try {
         data = await resend.emails.send({
-            from: "Contact Form <onboarding@resend.com>",
+            from: "Contact Form <onboarding@resend.dev>",
             to: "mohanka2102@gmail.com",
             subject: "Message from contact form",
             reply_to: senderEmail,
